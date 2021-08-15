27b95fd74878b2a70a71739da3192e9f2d3a2492
Erik Gaal <me@erikgaal.nl>
2021-08-15 02:54:01 +0200

    tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: ['index.html', 'src/**/*.{js,ts,jsx,tsx}'],
+  purge: ["index.html", "src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: colors.yellow,
+        orange: colors.orange,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
